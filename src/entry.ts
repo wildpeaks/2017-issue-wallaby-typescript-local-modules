3e699eb4e24e6d7eb9627b18b5901573d04a2f17
Cecile Muller <git@wildpeaks.fr>
2017-06-20 07:45:14 +0200

    Added a simple example

---
+++ src/entry.ts
@@ -0,0 +1,7 @@
+
+// works in both, but skips package.json
+import hello from 'examples/hello';
+import helloworld from 'examples/helloworld';
+
+console.log(hello());
+console.log(helloworld());
---
+++ wallaby.js
@@ -0,0 +1,19 @@
+
+module.exports = () => {
+	return {
+		debug: true,
+		testFramework: 'jasmine',
+		files: [
+			'src/**/*.ts',
+			{pattern: 'src/**/package.json', instrument: false, load: false},
+			// {pattern: 'src/**/package.json', instrument: false, load: false},
+			'!src/**/*.spec.ts'
+		],
+		tests: [
+			'src/**/*.spec.ts'
+		],
+		env: {
+			type: 'node',
+		}
+	};
+};
